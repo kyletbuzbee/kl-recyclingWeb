5ce4d3dc5fdf073b3666344aefa8bac62da80d38
Kyle Buzbee <kyle.thomas.buzbee@gmail.com>
2025-10-10 18:21:58 -0500

    ui: update service messaging to remove '56+ years service' references
    - Changed '56 Years Experience' to 'Years of Excellence' in trust metrics
    - Changed '56 Years: Serving Texas since 1969' to 'Family-Owned Since 1969'
    - No '10,000+ tons recycled annually' found in current hero component

--- src/components/TrustBuilder.tsx
+++ src/components/TrustBuilder.tsx
@@ -34,8 +34,8 @@ const TrustBuilder: FC = (): JSX.Element => {
       color: "green",
     },
     {
-      value: 68,
-      label: "Years Experience",
+      value: trustworthinessData.years,
+      label: "Years of Excellence",
       icon: "🏛️",
       color: "blue",
     },
@@ -166,7 +166,7 @@ const TrustBuilder: FC = (): JSX.Element => {
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                 </svg>
                 <span className="font-bold text-lg">
-                  {trustworthinessData.years} Years: {trustworthinessData.since} {trustworthinessData.year}
+                  Family-Owned Since {trustworthinessData.year}
                 </span>
               </div>
 
